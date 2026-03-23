d9b9a789e5895ed3626b2028d71cfcb332901020
sample tarou <sample@example.com>
2026-03-23 11:25:05 +0900

    不要な.jsxファイルを削除。メインのファイルの名前を修正

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,12 @@
-import { useEffect, useState } from "react";
 import Title from "./Title";
 import InputArea from "./components/InputArea";
-import TodoList from "./components/TodoList";
-import TodoSummary from "./components/TodoSummary";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { text: "Reactを学ぶ", isCompleted: true },
-    { text: "買い物に行く", isCompleted: false },
-    { text: "歯医者に行く", isCompleted: true },
-  ]);
-
   return (
-    <>
+    <div id="root">
       <Title text="ToDo List" />
       <InputArea />
-      <TodoList todos={todos} />
-      <TodoSummary />
-    </>
+    </div>
   );
 }
 
--- src/components/TodoSummary.jsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function TodoSummary() {
-  return <div>全タスク</div>;
-}
